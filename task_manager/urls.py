0f5667ba5c3bc970e37b58ea4a6ce6d98456a46d
raev_va <vlrkors@gmail.com>
2025-10-30 06:52:11 +0300

    Add or update files  Stage3

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import include, path
 
 from task_manager import views
 from task_manager.users.views import UserLoginView, UserLogoutView
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.IndexView.as_view(), name='index'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
