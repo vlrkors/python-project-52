a2855047ae54ecccba036e66914cd65fac4da420
raev_va <vlrkors@gmail.com>
2025-11-14 06:46:37 +0300

    Feat python modeles Stage

--- build.sh
+++ build.sh
@@ -6,7 +6,4 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 source "$HOME/.local/bin/env"
 
 # Install dependencies & prepare project.
-# В окружении сборки Render нет доступа к базе, поэтому миграции выполняются
-# перед запуском сервиса (см. цель render-start). Здесь ограничиваемся
-# установкой зависимостей и сборкой статики.
 make install && make collectstatic
