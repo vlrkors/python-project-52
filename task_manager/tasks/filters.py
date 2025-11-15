29fe7aff7838a284f5cbba42b5c3877c7921bfdb
raev_va <vlrkors@gmail.com>
2025-11-15 07:08:31 +0300

    Feat: add or refactor code. Step 5. Refactor task filters

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -1,4 +1,4 @@
-from django import forms
+import django_filters
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 
@@ -10,57 +10,20 @@ from .models import Task
 User = get_user_model()
 
 
-class TaskFilterForm(forms.Form):
-    status = forms.ModelChoiceField(
+class TaskFilter(django_filters.FilterSet):
+    status = django_filters.ModelChoiceFilter(
         queryset=Status.objects.all(),
-        label=_("Status"),
-        required=False,
+        label=_("Status")
     )
-    executor = forms.ModelChoiceField(
+    executor = django_filters.ModelChoiceFilter(
         queryset=User.objects.all(),
-        label=_("Executor"),
-        required=False,
+        label=_("Executor")
     )
-    labels = forms.ModelChoiceField(
+    labels = django_filters.ModelChoiceFilter(
         queryset=Label.objects.all(),
-        label=_("Label"),
-        required=False,
+        label=_("Label")
     )
 
-
-class TaskFilter:
-    """Простая подмена django-filter, достаточная для тестов проекта."""
-
-    def __init__(self, data=None, queryset=None):
-        self.queryset = (
-            queryset if queryset is not None else Task.objects.none()
-        )
-        self.form = TaskFilterForm(data or None)
-        self._qs = self._apply_filters()
-
-    def _apply_filters(self):
-        if self.form.is_bound:
-            if not self.form.is_valid():
-                return self.queryset
-            cleaned_data = self.form.cleaned_data
-        else:
-            cleaned_data = {}
-        queryset = self.queryset
-
-        status = cleaned_data.get("status")
-        if status:
-            queryset = queryset.filter(status=status)
-
-        executor = cleaned_data.get("executor")
-        if executor:
-            queryset = queryset.filter(executor=executor)
-
-        label = cleaned_data.get("labels")
-        if label:
-            queryset = queryset.filter(labels=label)
-
-        return queryset
-
-    @property
-    def qs(self):
-        return self._qs
+    class Meta:
+        model = Task
+        fields = ['status', 'executor', 'labels']
