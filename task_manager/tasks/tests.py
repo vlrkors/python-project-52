b8bc55768e7e929e2598399fac4c289963c231bc
raev_va <vlrkors@gmail.com>
2025-11-17 14:12:18 +0300

    Feat: add code for Stage 6

--- task_manager/tasks/tests.py
+++ task_manager/tasks/tests.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.messages import get_messages
 from django.urls import reverse
 
+from task_manager.labels.models import Label
 from task_manager.statuses.models import Status
 
 from .models import Task
@@ -22,6 +23,8 @@ class TestTaskCRUD:
             password='pass123'  # NOSONAR
         )
         self.status = Status.objects.create(name='in_progress')
+        self.label_bug = Label.objects.create(name='Bug')
+        self.label_feature = Label.objects.create(name='Feature')
 
     def test_create_task(self, client):
         client.login(username='user1', password='pass123')  # NOSONAR
@@ -31,27 +34,39 @@ class TestTaskCRUD:
             'description': 'Some description',
             'status': self.status.id,
             'executor': self.user2.id,
+            'labels': [self.label_bug.id, self.label_feature.id],
         }
         response = client.post(url, data)
         assert response.status_code == 302
         assert Task.objects.count() == 1
-        assert Task.objects.first().author == self.user1
+        task = Task.objects.first()
+        assert task.author == self.user1
+        assert set(task.labels.values_list('id', flat=True)) == {
+            self.label_bug.id,
+            self.label_feature.id,
+        }
 
     def test_update_task(self, client):
         task = Task.objects.create(
             name='Old Task', status=self.status,
             author=self.user1,
         )
+        task.labels.add(self.label_bug)
         client.login(username='user1', password='pass123')  # NOSONAR
         url = reverse('task_update', kwargs={'pk': task.id})
         response = client.post(url, {
             'name': 'Updated Task',
             'description': '',
             'status': self.status.id,
+            'executor': self.user2.id,
+            'labels': [self.label_feature.id],
         })
         assert response.status_code == 302
         task.refresh_from_db()
         assert task.name == 'Updated Task'
+        assert list(task.labels.values_list('id', flat=True)) == [
+            self.label_feature.id
+        ]
 
     def test_delete_task_by_author(self, client):
         task = Task.objects.create(
