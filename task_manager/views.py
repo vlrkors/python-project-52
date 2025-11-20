78647db83ef9cb5d528387db94498f6c8ee371ce
raev_va <vlrkors@gmail.com>
2025-11-20 12:15:20 +0300

    Add code or fix bugs from Stage7

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,10 @@
 from django.views.generic import TemplateView
 
+
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get(self, request, *args, **kwargs):
+        raise Exception("Rollbar test")
+        return super().get(request, *args, **kwargs)
 
