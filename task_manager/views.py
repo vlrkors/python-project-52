3e1632dd2caeed000184fb3bc39e8b19495ad8f2
raev_va <vlrkors@gmail.com>
2025-11-21 18:31:05 +0300

    Add cofe or refact Project52 Stage Last

--- task_manager/views.py
+++ task_manager/views.py
@@ -4,6 +4,5 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
