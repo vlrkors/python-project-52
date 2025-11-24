b93916ab4271f0f7be8cb9637494aaa76cb3f2be
raev_va <vlrkors@gmail.com>
2025-11-24 18:41:07 +0300

    Add or refact code to Stage8

--- task_manager/rollbar_middleware.py
+++ task_manager/rollbar_middleware.py
@@ -1,8 +1,9 @@
+from django.conf import settings
+from django.utils.deprecation import MiddlewareMixin
+
 try:
     from rollbar.contrib.django.middleware import RollbarNotifierMiddleware
 except Exception:  # pragma: no cover
-    from django.utils.deprecation import MiddlewareMixin
-
     class RollbarNotifierMiddleware(MiddlewareMixin):
         """Базовый no-op middleware, когда rollbar недоступен."""
 
@@ -16,6 +17,18 @@ except Exception:  # pragma: no cover
 
 
 class CustomRollbarNotifierMiddleware(RollbarNotifierMiddleware):
+    def __init__(self, get_response=None):
+        rollbar_settings = getattr(settings, 'ROLLBAR', {}) or {}
+
+        if not rollbar_settings.get('access_token'):
+            # Если токен не задан, пропускаем инициализацию rollbar,
+            # но оставляем middleware рабочим (например, в тестах).
+            MiddlewareMixin.__init__(self, get_response)
+            self.settings = rollbar_settings
+            return
+
+        super().__init__(get_response)
+
     def get_extra_data(self, request, exc):
         extra_data = dict()
         extra_data['feature_flags'] = ['feature_1', 'feature_2']
