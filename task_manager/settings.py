b69675681c14d8d22cf73d8031faf16a7cafb189
raev_va <vlrkors@gmail.com>
2025-11-24 18:20:09 +0300

    Add or Refactor Project52 Stage8

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -217,7 +217,11 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": (
+            "django.contrib.staticfiles.storage.StaticFilesStorage"
+            if DEBUG or IS_TESTING
+            else "whitenoise.storage.CompressedManifestStaticFilesStorage"
+        ),
     },
 }
 
