f312f4e3f9086cd08d8b030ba50f0d0de7e6734e
raev_va <vlrkors@gmail.com>
2025-11-24 06:47:18 +0300

    Add or refact code from Project52 Stage Last

--- task_manager/labels/forms.py
+++ task_manager/labels/forms.py
@@ -1,9 +1,21 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 from .models import Label
 
 
 class LabelForm(forms.ModelForm):
+    label_suffix = ""
+
     class Meta:
         model = Label
-        fields = ['name']
+        fields = ["name"]
+        labels = {"name": _("Name")}
+        widgets = {
+            "name": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": _("Name"),
+                },
+            ),
+        }
